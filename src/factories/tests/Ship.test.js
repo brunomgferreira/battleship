dbd4a76788d4b53afe2522abe3bb46c0c9220ced
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-09-01 14:40:49 +0100

    ship is done

--- src/factories/tests/Ship.test.js
+++ src/factories/tests/Ship.test.js
@@ -1,31 +1,36 @@
 import Ship from '../Ship'
 
-beforeEach(() => {
-    ship = new Ship(3);
-});
+describe('Ship', () => {
+    let ship = new Ship(3)
 
-test('creates and initializes a ship', () => {
-    expect(ship).toEqual({ length: 3, hits: []});
-});
-
-test('takes a hit', () => {
-    ship.hit(2);
-    expect(ship.hits).toContain(2);
-});
-
-test('sinks', () => {
-    ship.hit(0);
-    ship.hit(1);
-    ship.hit(2);
-    expect(ship.isSunk()).toBe(true);
-});
-
-test('prevents being hit multiple times at the same spot', () => {
-    ship.hit(1);
-    ship.hit(1);
-    ship.hit(1);
-    expect(ship.hits.length).toBe(1);
-});
-
-
-
+    beforeEach(() => {
+        ship = new Ship(3);
+    });
+    
+    test('creates and initializes a ship', () => {
+        expect(ship).toEqual({ length: 3, hits: []});
+    });
+    
+    test('takes a hit', () => {
+        ship.hit(2);
+        expect(ship.hits).toContain(2);
+    });
+    
+    test('sinks', () => {
+        ship.hit(0);
+        ship.hit(1);
+        ship.hit(2);
+        expect(ship.isSunk()).toBe(true);
+    });
+    
+    test('prevents being hit multiple times at the same spot', () => {
+        ship.hit(1);
+        ship.hit(1);
+        ship.hit(1);
+        expect(ship.hits.length).toBe(1);
+    });
+    
+    
+    
+    
+})
